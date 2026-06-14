5f141af06d758203e4f91e87b66c5d97cf8554fe
zrygan <zr.gntn@gmail.com>
2026-06-15 02:55:27 +0800

    Removed util files and add ruff for formatting

--- src/util.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-def season_from_month(m):
-    """Meteorological seasons for the northern hemisphere."""
-    return {12: "winter", 1: "winter", 2: "winter",
-            3: "spring", 4: "spring", 5: "spring",
-            6: "summer", 7: "summer", 8: "summer",
-            9: "fall", 10: "fall", 11: "fall"}.get(m, np.nan)
